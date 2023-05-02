c94550e79325f9c303ad3e451d34cfa7db2e5a26
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-02 23:51:13 +0600

    Json data added

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const port = process.env.port || 5000;
+
+const chefs = require('./data/chefs.json');
+app.use(cors());
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+});
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const singleChef = chefs.find(n => n.id == id)
+    res.send(singleChef)
+});
 
 app.get('/', (req, res) => {
     res.send('Food-Network Here..')
